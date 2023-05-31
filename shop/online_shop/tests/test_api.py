6018c5900133f3b70a6d064b05630cbf852cb8a2
Viachaslau <slava@informatics.by>
2023-05-31 17:41:51 +0300

    shop_online, clients -> some optimization tests

--- shop/online_shop/tests/test_api.py
+++ shop/online_shop/tests/test_api.py
@@ -7,47 +7,41 @@ from online_shop.serializers import ProductSerializer
 
 
 class ProductApiTestCase(APITestCase):
-    def test_get(self):
-        category_1 = Category.objects.create(name='Bathroom', slug='bathroom')
+    def setUp(self):
+        self.category_1 = Category.objects.create(name='Bathroom', slug='bathroom')
 
-        product_1 = Product.objects.create(name='Toothpaste', slug='toothpaste', category=category_1,
+        self.product_1 = Product.objects.create(name='Toothpaste', slug='toothpaste', category=self.category_1,
                                    description='Paste for tooth',
                                    price=5, stock=5, available=True)
 
-        category_2 = Category.objects.create(name='Kitchen', slug='kitchen')
-        product_2 = Product.objects.create(name='Towel', slug='towel', category=category_2,
+        self.category_2 = Category.objects.create(name='Kitchen', slug='kitchen')
+        self.product_2 = Product.objects.create(name='Towel', slug='towel', category=self.category_2,
                                            description='Hand towel',
                                            price=2, stock=15, available=True)
+        self.url_list = reverse('product-list')
 
-        url = reverse('product-list')
+    def test_get(self):
 
-        response = self.client.get(url)
+        response = self.client.get(self.url_list)
 
-        serializer_data = ProductSerializer([product_1, product_2], many=True).data
+        serializer_data = ProductSerializer([self.product_1, self.product_2], many=True).data
 
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(serializer_data, response.data)
 
     def test_list_product_view_performance(self):
-        category_1 = Category.objects.create(name='Bathroom', slug='bathroom')
-
-        Product.objects.create(name='Toothpaste', slug='toothpaste', category=category_1,
-                                   description='Paste for tooth',
-                                   price=5, stock=5, available=True)
-
-        url = reverse('product-list')
 
         with self.assertNumQueries(1):
-            response = self.client.get(url)
-
-        self.assertEqual(len(response.data), 1)
-
-        category_2 = Category.objects.create(name='Kitchen', slug='kitchen')
-        Product.objects.create(name='Towel', slug='towel', category=category_2,
-                                           description='Hand towel',
-                                           price=2, stock=15, available=True)
-
-        with self.assertNumQueries(1):
-            response = self.client.get(url)
+            response = self.client.get(self.url_list)
 
         self.assertEqual(len(response.data), 2)
+
+        category_3 = Category.objects.create(name='Test', slug='test')
+        Product.objects.create(name='Towel', slug='towel', category=category_3,
+                                           description='Hand towel',
+                                           price=20, stock=10, available=True)
+
+        with self.assertNumQueries(1):
+            response = self.client.get(self.url_list)
+
+        self.assertEqual(len(response.data), 3)
